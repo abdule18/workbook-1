bdacf3cba5d32993e1073821c0f6acf4ffd4438f
abdule18 <101951085+abdule18@users.noreply.github.com>
2025-04-16 11:01:09 -0400

    update the sandwich shop and added a loaded option

--- SandwichShop/src/main/java/com/pluralsight/Main.java
+++ SandwichShop/src/main/java/com/pluralsight/Main.java
@@ -1,19 +1,33 @@
 package com.pluralsight;
+
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Choose your sandwich:\n" +
                 "1: Regular: base price $5.45\n" +
                 "2: Large: base price $8.95\n");
         int choice = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Would you like the sandwich loaded (yes/no):\n" +
+                "Regular: base price $1.00\n" +
+                "Large: base price $1.75\n");
+        String secondChoice = scanner.nextLine();
 
         double price;
-        if (choice == 1){
+
+        if (choice == 1 && secondChoice.equals("yes")){
+            price = 6.45;
+        } else if(choice == 2 && secondChoice.equals("yes")){
+            price = 10.07;
+        } else if (choice == 1 && secondChoice.equals("no")){
             price = 5.45;
-        } else if(choice == 2){
+        } else if (choice == 2 && secondChoice.equals("no")){
             price = 8.95;
         } else {
             System.out.println("Invalid input");
