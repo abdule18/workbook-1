c458a69f075b0377a326e13e499575910ba2b9ac
abdule18 <101951085+abdule18@users.noreply.github.com>
2025-04-16 08:24:17 -0400

    the update

--- PayrollCalculator/src/main/java/com/pluralsight/Main.java
+++ PayrollCalculator/src/main/java/com/pluralsight/Main.java
@@ -9,13 +9,28 @@ public class Main {
         String name = scanner.nextLine();
 
         System.out.print("How many hour did you worked? ");
-        double hours = scanner.nextDouble();
+        float hours = scanner.nextFloat();
 
         System.out.print("What is your hourly pay? ");
         double pay = scanner.nextDouble();
 
         double grossIncome = (pay * hours) * 52;
 
+        double grossPay;
+
+        if (hours <= 40){
+            grossPay = hours * pay;
+        } else {
+            float overtimeHours = hours - 40;
+            double basePay = 40 * pay;
+            double overtimeRate = pay * 1.5;
+            double overtimePay = overtimeHours * overtimeRate;
+
+            grossPay = basePay + overtimePay;
+
+        }
+
+        System.out.println(grossPay);
         System.out.println(name);
         System.out.println(grossIncome);
 
